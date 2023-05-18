ed09ac80b5e41e86e5bea8c046158f545c66340f
Brian <linbrian24@gmail.com>
2023-05-18 17:02:32 -0400

    Add custom colors and google font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'red': 'hsl(1, 90%, 64%)',
+        'blue': 'hsl(219, 85%, 26%)',
+        'white': 'hsl(0, 0%, 100%)',
+        'vl-grayish-blue': 'hsl(210, 60%, 98%)',
+        'l-grayish-blue-1': 'hsl(211, 68%, 94%)',
+        'l-grayish-blue-2': 'hsl(205, 33%, 90%)',
+        'grayish-blue': 'hsl(219, 14%, 63%)',
+        'dark-grayish-blue': 'hsl(219, 12%, 42%)',
+        'vd-grayish-blue': 'hsl(224, 21%, 14%)',
+
+      }
+    },
   },
   plugins: [],
 }
